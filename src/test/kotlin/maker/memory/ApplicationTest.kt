420a342cfdfe326e2a4aada64cb7077f3183cd06
Vadim Arashin <v.arashin@icloud.com>
2021-08-19 09:55:50 +0300

    first commit

---
+++ src/test/kotlin/maker/memory/ApplicationTest.kt
@@ -0,0 +1,13 @@
+package maker.memory
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import maker.memory.plugins.*
+
+class ApplicationTest {
+}
