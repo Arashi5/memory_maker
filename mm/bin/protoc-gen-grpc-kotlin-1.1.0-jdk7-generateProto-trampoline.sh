66838264163abfc87c67b80c6ae97a37c4067598
Vadim Arashin <v.arashin@icloud.com>
2021-08-24 17:12:43 +0300

    delete

--- mm/bin/protoc-gen-grpc-kotlin-1.1.0-jdk7-generateProto-trampoline.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec '/home/arashi5p/Downloads/idea-IU-211.6693.111/jbr/bin/java' -jar '/home/arashi5p/.gradle/caches/modules-2/files-2.1/io.grpc/protoc-gen-grpc-kotlin/1.1.0/bd2055d9d381f0dc2cdbf0c5a7b71493bb1ce8f9/protoc-gen-grpc-kotlin-1.1.0-jdk7.jar' "$@"
